e153815206c0fc39b840bb512cf44a9bc10381ae
Alexnorvag <norvag@mail.ru>
2018-06-15 23:25:02 +0300

    init

---
+++ src/App/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+import Homepage from '../Homepage/Homepage';
+// import Moore from '../MachinePage/Moore'
+// import Mealy from '../MachinePage/Mealy'
+import './App.scss';
+
+// Components
+import { Root } from "./Root";
+
+class App extends Component {
+    render() {
+        // const {footer} = this.props;
+
+        return (
+            <div className="app py-0">
+                <Router>
+                    <Root>
+                        <main>
+                            <Switch>
+                                <Route path={'/home'} component={Homepage}/>
+                                {/* <Route path={'/moore'} component={Moore}/> */}
+                                {/* <Route path={'/mealy'} component={Mealy}/> */}
+                                <Route exact path={'/boolean-superposition'} component={() => <Redirect to={'/home'} />} />
+                                <Route exact path={'/'} component={() => <Redirect to={'/home'} />} />
+                            </Switch>
+                        </main>
+                    </Root>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Homepage/Homepage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link, withRouter} from 'react-router-dom'; // eslint-disable-line no-unused-vars
+import './Homepage.scss';
+
+class Homepage extends Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+
+        return (
+            <div className="homepage content">
+                HOMEPAGE
+            </div>
+        );
+    }
+}
+
+export default Homepage;
